8e71b62eb53fa6d619bf537d11c0e4270ca37c6e
ShutenkoStas <shotenko494@gmail.com>
2025-11-03 21:52:20 +0200

    Make app

---
+++ forpractic/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ForpracticConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'forpractic'
---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+import os
+
+import django
+
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+django.setup()
